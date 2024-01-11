e79b78bfc99b7718679845cedecef0762e79e5ec
Aleksandr Babkin <ababkin@ptsecurity.com>
2024-01-12 01:26:19 +0300

    добавлен шаблон передачи параметров в методы

--- test_add_group.py
+++ test_add_group.py
@@ -9,6 +9,7 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from group import Group
 
 
 class TestAddGroup:
@@ -20,22 +21,47 @@ class TestAddGroup:
         self.driver.quit()
 
     def test_add_group(self):
-        self.driver.get("http://localhost/addressbook/")
-        self.driver.set_window_size(753, 352)
-        self.driver.find_element(By.NAME, "user").send_keys("admin")
-        self.driver.find_element(By.NAME, "pass").send_keys("secret")
-        self.driver.find_element(By.ID, "LoginForm").click()
-        self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(7)").click()
-        self.driver.find_element(By.LINK_TEXT, "groups").click()
+        self.open_home_page()
+        self.login(username="admin", password="secret")
+        self.open_groups_page()
+        self.create_group(
+            Group(name="Бегимоты", header="Живут", footer="В болоте"))
+        self.open_groups_page()
+        self.logout()
+
+    def test_add_empty_group(self):
+        self.open_home_page()
+        self.login(username="admin", password="secret")
+        self.open_groups_page()
+        self.create_group(
+            Group(name="", header="", footer=""))
+        self.open_groups_page()
+        self.logout()
+
+    def logout(self):
+        self.driver.find_element(By.LINK_TEXT, "Logout").click()
+
+    def create_group(self, group):
         self.driver.find_element(By.NAME, "new").click()
         self.driver.find_element(By.NAME, "group_name").click()
-        self.driver.find_element(By.NAME, "group_name").send_keys("Бегимоты")
+        self.driver.find_element(By.NAME, "group_name").send_keys(group.name)
         self.driver.find_element(By.NAME, "group_header").click()
-        self.driver.find_element(By.NAME, "group_header").send_keys("Живут")
+        self.driver.find_element(By.NAME, "group_header").send_keys(
+            group.header)
         self.driver.find_element(By.NAME, "group_footer").click()
-        self.driver.find_element(By.NAME, "group_footer").send_keys("В болоте")
+        self.driver.find_element(By.NAME, "group_footer").send_keys(
+            group.footer)
         self.driver.find_element(By.NAME, "submit").click()
+
+    def open_groups_page(self):
         self.driver.find_element(By.LINK_TEXT, "groups").click()
-        self.driver.find_element(By.LINK_TEXT, "Logout").click()
-        self.driver.find_element(By.NAME, "user").send_keys("admin")
-        self.driver.find_element(By.NAME, "pass").send_keys("secret")
+
+    def login(self, username, password):
+        self.driver.set_window_size(753, 352)
+        self.driver.find_element(By.NAME, "user").send_keys(username)
+        self.driver.find_element(By.NAME, "pass").send_keys(password)
+        self.driver.find_element(By.ID, "LoginForm").click()
+        self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(7)").click()
+
+    def open_home_page(self):
+        self.driver.get("http://localhost/addressbook/")
