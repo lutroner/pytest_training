ecba5f0f775a61ea1f49ed386e72433c2253f147
Aleksandr Babkin <ababkin@ptsecurity.com>
2024-01-20 01:02:06 +0300

    созданы пакеты для разных зон ответственности

---
+++ test/test_add_group.py
@@ -0,0 +1,23 @@
+import pytest
+
+from pytest_training.fixture.application import Application
+from pytest_training.model.group import Group
+
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    yield fixture
+
+
+def test_add_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="Бегимоты", header="Живут", footer="В болоте"))
+    app.logout()
+
+
+def test_add_empty_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
