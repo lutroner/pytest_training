4831a1958e0885968c26830bd725940acaf4ec5e
Aleksandr Babkin <ababkin@ptsecurity.com>
2024-01-20 01:31:11 +0300

    добавлен класс для управления сессиями

--- fixture/application.py
+++ fixture/application.py
@@ -1,15 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+from fixture.session import SessionHelper
+
 
 class Application:
     def __init__(self):
         self.driver = webdriver.Chrome()
         self.driver.set_window_size(800, 600)
         self.driver.implicitly_wait(60)
-
-    def logout(self):
-        self.driver.find_element(By.LINK_TEXT, "Logout").click()
+        self.session = SessionHelper(self)
 
     def create_group(self, group):
         self.open_groups_page()
@@ -24,12 +24,6 @@ class Application:
 
     def open_groups_page(self):
         self.driver.find_element(By.LINK_TEXT, "groups").click()
-
-    def login(self, username, password):
-        self.open_home_page()
-        self.driver.find_element(By.NAME, "user").send_keys(username)
-        self.driver.find_element(By.NAME, "pass").send_keys(password)
-        self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(7)").click()
 
     def open_home_page(self):
         self.driver.get("http://localhost/addressbook/")
